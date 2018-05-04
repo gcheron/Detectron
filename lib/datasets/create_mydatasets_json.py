1df62a311164f2066ab17166a8931b7f9160ec76
gcheron <gcheron@gpu113.eth.cluster>
2018-05-04 20:43:50 +0200

    save sampled keyframe list

--- lib/datasets/create_mydatasets_json.py
+++ lib/datasets/create_mydatasets_json.py
@@ -14,14 +14,15 @@ def get_UCF101(setname):
                      'VolleyballSpiking', 'WalkingWithDog')
    gtpath = datasetroot + '/detection/gtfile.pkl'
 
+   resroot = datasetroot + '/detection/instances_%s1_ucf101' % setname
+   if setname == 'val':
+      setname = 'test'
    trainlist = datasetroot + '/detection/OF_vidlist_%s1.txt' % setname
 
    onkeyframes = False
    check_WH = False
 
-   respath = datasetroot + '/detection/instances_%s1_ucf101.json' % setname
-
-   return class_list, gtpath, trainlist, onkeyframes, check_WH, respath
+   return class_list, gtpath, trainlist, onkeyframes, check_WH, resroot
 
 
 def get_DALY(setname):
@@ -30,17 +31,19 @@ def get_DALY(setname):
                      'FoldingTextile', 'Ironing', 'Phoning', 'PlayingHarmonica', 'TakingPhotosOrVideos')
    gtpath = datasetroot + '/gtfile.pkl'
 
+   resroot = datasetroot + '/instances_%s1_daly' % setname
+   if setname == 'val':
+      setname = 'test'
    trainlist = datasetroot + '/OF_vidlist_%s1.txt' % setname
 
    onkeyframes = True
    check_WH = True
 
-   respath = datasetroot + '/instances_%s1_daly.json' % setname
 
-   return class_list, gtpath, trainlist, onkeyframes, check_WH, respath
+   return class_list, gtpath, trainlist, onkeyframes, check_WH, resroot
 
 
-def get_gt_instance_annot(inst, onkeyframes):
+def get_gt_instance_annot(inst, onkeyframes, nkeyframes):
    if onkeyframes:
       boxes = []
       frames = []
@@ -73,15 +76,44 @@ def get_gt_instance_annot(inst, onkeyframes):
    areas = W * H
 
    assert len(frames) == boxes.shape[0] and boxes.shape[1] == 4
-   return boxes, frames, classes, areas
+
+   keyframes = {'inst': inst}
+   if nkeyframes > 0:
+      perm = np.random.permutation(len(frames))
+      keep = perm[:nkeyframes]
+      boxes = boxes[keep, :]
+      frames = np.array(frames)[keep].tolist()
+      classes = np.array(classes)[keep].tolist()
+      areas = areas[keep]
+      keyframes['boxes'] = boxes
+      keyframes['frames'] = frames
+      keyframes['idx'] = keep
+
+   return boxes, frames, classes, areas, keyframes
 
 # coco example
 #cocodataset = json.load(open('/sequoia/data2/jpeyre/datasets/coco/annotations/instances_train2014.json', 'r'))
+np.random.seed(19)
 
-setname = 'test' # train | test
-get_fun = get_UCF101 # get_DALY | get_UCF101
 
-class_list, gtpath, trainlist, onkeyframes, check_WH, respath = get_fun(setname)
+setname = 'train' # train | val
+dataset_name = 'ucf101' # ucf101 | daly
+nkeyframes = -1 # -1 | 1 | 3
+
+if dataset_name == 'ucf101':
+   get_fun = get_UCF101
+elif dataset_name == 'daly':
+   get_fun = get_DALY
+else:
+   raise ValueError
+
+class_list, gtpath, trainlist, onkeyframes, check_WH, resroot = get_fun(setname)
+
+respath = resroot
+if nkeyframes > 0:
+   respath += '_keyframes%d' % (nkeyframes)
+reskeylist = respath + '_keylist.pkl'
+respath += '.json'
 
 # get ground truth
 with open(gtpath) as f:
@@ -100,6 +132,8 @@ for c, class_name in enumerate(class_list):
 
 
 imname2id = {}
+numinstance = 0
+all_keyframes = {}
 for v, vid in enumerate(vidlist): # for all videos from the split
    # fill the following:
    # annotations: ['area', 'image_id', 'bbox', 'category_id', 'id']
@@ -110,6 +144,7 @@ for v, vid in enumerate(vidlist): # for all videos from the split
 
    # get video gt
    vid_gt = gtfile[vid]
+   all_keyframes[vid] = {'instances': [], 'allinfo': vid_gt}
 
    vlen = vid_gt['length']
 
@@ -119,8 +154,12 @@ for v, vid in enumerate(vidlist): # for all videos from the split
       imW, imH = 320, 240
 
 
-   for inst in vid_gt['gts']: # for all video GT instances
-      boxes, frames, classes, areas = get_gt_instance_annot(inst, onkeyframes) 
+   for i_inst, inst in enumerate(vid_gt['gts']): # for all video GT instances
+      numinstance += 1
+      boxes, frames, classes, areas, keyframes = get_gt_instance_annot(inst, onkeyframes, nkeyframes)
+
+      keyframes['inst_id'] = i_inst
+      all_keyframes[vid]['instances'].append(keyframes)
 
       for i, f in enumerate(frames):
          frame_name = '%s/image-%05d.jpg' % (vid, f)
@@ -144,4 +183,8 @@ for v, vid in enumerate(vidlist): # for all videos from the split
 with open(respath, 'w') as f:
    json.dump(dataset, f)
 
-print 'Saved %d annotations (on %d images) to:\n%s' % (len(dataset['annotations']), len(dataset['images']), respath)
+if nkeyframes > 0:
+   with open(reskeylist, 'w') as f:
+      pickle.dump(all_keyframes, f)
+
+print 'Saved %d annotations (on %d images and %d instances) to:\n%s' % (len(dataset['annotations']), len(dataset['images']), numinstance, respath)
